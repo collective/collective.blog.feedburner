91faf7ae8cfc1b2f5f26b10391bf37eb112d1c00
Chrissy Wainwright <chrissy@sixfeetup.com>
2015-06-09 13:48:19 -0400

    make update for Plone 4.3

--- collective/blog/feedburner/viewlets.py
+++ collective/blog/feedburner/viewlets.py
@@ -1,5 +1,5 @@
 from collective.blog.feeds.viewlets import FeedsViewlet as ViewletBase
-from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 
 class FeedsViewlet(ViewletBase):
     render = ViewPageTemplateFile('feedsviewlet.pt')
